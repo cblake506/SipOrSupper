8fd0130bf5195fcfbe3367518aa0cdfcfa7bffd3
Reyes Martinez <reyes.martinez99@gmail.com>
2022-04-30 10:26:40 -0500

    final page

--- foodScript.js
+++ foodScript.js
@@ -1,32 +1,27 @@
-var ageValid = localStorage.getItem('ageValid');
-
-if(!ageValid || ageValid === 'false'){
-  $('.ui.basic.modal')
-    .modal('show')
-  ;
-} else{
-  $('.drink-section').css('display', 'block');
-  $('.drink-section-button').css('display', 'inline-block');
+var ageValid = localStorage.getItem("ageValid");
+if (!ageValid || ageValid === "false") {
+  $(".ui.basic.modal").modal("show");
+} else {
+  $(".drink-section").css("display", "block");
+  $(".drink-section-button").css("display", "inline-block");
 }
-$('#ageFalse').on('click', setAgeFalse);
-$('#ageTrue').on('click', setAgeTrue);
-
+$("#ageFalse").on("click", setAgeFalse);
+$("#ageTrue").on("click", setAgeTrue);
 function setAgeFalse() {
-  localStorage.setItem('ageValid', 'false');
+  localStorage.setItem("ageValid", "false");
   return;
 }
 function setAgeTrue() {
-  localStorage.setItem('ageValid', 'true');
-  $('.drink-section').css('display', 'block');
-  $('.drink-section-button').css('display', 'inline-block');
+  localStorage.setItem("ageValid", "true");
+  $(".drink-section").css("display", "block");
+  $(".drink-section-button").css("display", "inline-block");
   return;
 }
-
 function writeDrinkTitle() {
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php", {
     // The browser fetches the resource from the remote server without first looking in the cache.
     // The browser will then update the cache with the downloaded resource.
-    cache: 'reload',
+    cache: "reload",
   })
     .then(function (response) {
       return response.json();
@@ -37,47 +32,47 @@ function writeDrinkTitle() {
       let measurements1 = [];
       let beverage;
       beverage = data.drinks[0];
-
       for (var i = 1; i <= 15; i++) {
-        ingredients1.push(beverage['strIngredient' + i.toString()])
+        ingredients1.push(beverage["strIngredient" + i.toString()]);
       }
       for (var i = 1; i <= 15; i++) {
-        measurements1.push(beverage['strMeasure' + i.toString()])
+        measurements1.push(beverage["strMeasure" + i.toString()]);
       }
-
-      document.getElementById("title1").innerHTML = "<div>" + beverage.strDrink + "</div>";
-      document.getElementById("results1").innerHTML = '<ul id="drinkList"></ul>';
-      document.getElementById("drink-image").setAttribute('src', beverage.strDrinkThumb);
-
+      document.getElementById("title1").innerHTML =
+        "<h3>" + beverage.strDrink + "</h3>";
+      document.getElementById("drink-results").innerHTML =
+        '<br><div id="drinkList"></div>';
+      document
+        .getElementById("drink-image")
+        .setAttribute("src", beverage.strDrinkThumb);
       cleanInstructions1 = beverage.strInstructions;
       //display the instructions
-      document.getElementById("results1").insertAdjacentHTML('beforeend', '<h3> Instructions </h3>');
-      document.getElementById("results1").insertAdjacentHTML('beforeend', '<p>' + cleanInstructions1 + '<p>');
-
+      document
+        .getElementById("drink-results")
+        .insertAdjacentHTML("beforeend", "<h3> Instructions </h3>");
+      document
+        .getElementById("drink-results")
+        .insertAdjacentHTML("beforeend", "<p>" + cleanInstructions1 + "<p>");
       for (let i = 0; i < ingredients1.length; i++) {
         if (ingredients1[i] !== "" && ingredients1[i] !== null) {
-          let listElIngredients = document.createElement("li");
-          listElIngredients.innerHTML = measurements1[i] + " " + ingredients1[i];
+          let listElIngredients = document.createElement("div");
+          listElIngredients.innerHTML =
+            measurements1[i] + " " + ingredients1[i];
           document.getElementById("drinkList").appendChild(listElIngredients);
         }
       }
     });
 }
-
-
 function main1() {
   buttonEl = document.getElementById("get_drink");
-  buttonEl.addEventListener("click", writeDrinkTitle)
+  buttonEl.addEventListener("click", writeDrinkTitle);
 }
-
 main1();
-
-
 function writeMealTitle() {
-  fetch('https://www.themealdb.com/api/json/v1/1/random.php', {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php", {
     // The browser fetches the resource from the remote server without first looking in the cache.
     // The browser will then update the cache with the downloaded resource.
-    cache: 'reload',
+    cache: "reload",
   })
     .then(function (response) {
       return response.json();
@@ -88,60 +83,65 @@ function writeMealTitle() {
       let measurements = [];
       let dinner;
       dinner = data.meals[0];
-
       for (var i = 1; i <= 20; i++) {
-        ingredients.push(dinner['strIngredient' + i.toString()])
+        ingredients.push(dinner["strIngredient" + i.toString()]);
       }
       for (var i = 1; i <= 20; i++) {
-        measurements.push(dinner['strMeasure' + i.toString()])
+        measurements.push(dinner["strMeasure" + i.toString()]);
       }
-      
-      document.getElementById("title").innerHTML = "<div>" + dinner.strMeal + "</div>";
-      document.getElementById("results").innerHTML = '<ul id="foodList"></ul>'
-      document.getElementById("dinner-image").setAttribute('src', dinner.strMealThumb);
-      
-      if(dinner.strYoutube !== ""){
+      document.getElementById("title").innerHTML =
+        "<h3>" + dinner.strMeal + "</h3>";
+      document.getElementById("results").innerHTML =
+        '<br><div id="foodList"></div>';
+      document
+        .getElementById("dinner-image")
+        .setAttribute("src", dinner.strMealThumb);
+      if (dinner.strYoutube !== "") {
         //html embeded youtube video format
         firstHalfStr = '<iframe src="https://www.youtube.com//embed/';
         secondHalfStr = '"></iframe>';
-
         //extract the 11 digit video ID from the full link provided in the API
         //remove the start of the URL
-        youtubeUrl = dinner.strYoutube.replace('https://www.youtube.com/watch?v=', "");
+        youtubeUrl = dinner.strYoutube.replace(
+          "https://www.youtube.com/watch?v=",
+          ""
+        );
         //only keep the 11 digit ID and none of the additional identifiers
         youtubeUrl = youtubeUrl.slice(0, 11);
-
-        console.log(firstHalfStr + youtubeUrl + secondHalfStr)
-        document.getElementById("results").insertAdjacentHTML('beforeend', firstHalfStr + youtubeUrl + secondHalfStr);
+        console.log(firstHalfStr + youtubeUrl + secondHalfStr);
+        document
+          .getElementById("results")
+          .insertAdjacentHTML(
+            "beforeend",
+            firstHalfStr + youtubeUrl + secondHalfStr
+          );
       }
-
       //remove non-standard characters from the instructions
-      cleanInstructions = dinner.strInstructions.replace(/[\u{0080}-\u{FFFF}]/gu,"");
+      cleanInstructions = dinner.strInstructions.replace(
+        /[\u{0080}-\u{FFFF}]/gu,
+        ""
+      );
       //display the instructions
-      document.getElementById("results").insertAdjacentHTML('beforeend', '<h3> Instructions </h3>');
-      document.getElementById("results").insertAdjacentHTML('beforeend', '<p>' + cleanInstructions + '<p>');
-
-      
-      
-
+      document
+        .getElementById("results")
+        .insertAdjacentHTML("beforeend", "<h3> Instructions </h3>");
+      document
+        .getElementById("results")
+        .insertAdjacentHTML("beforeend", "<p>" + cleanInstructions + "<p>");
       for (let i = 0; i < ingredients.length; i++) {
         if (ingredients[i] !== "" && ingredients[i] !== null) {
-          let listElIngredients = document.createElement("li");
+          let listElIngredients = document.createElement("div");
           listElIngredients.innerHTML = measurements[i] + " " + ingredients[i];
           document.getElementById("foodList").appendChild(listElIngredients);
         }
       }
     });
 }
-
 function main() {
   buttonEl = document.getElementById("get_dinner");
-  buttonEl.addEventListener("click", writeMealTitle)
+  buttonEl.addEventListener("click", writeMealTitle);
 }
-
 main();
-
-var getFoodAndDrink = document.getElementById('get_both');
-
+var getFoodAndDrink = document.getElementById("get_both");
 getFoodAndDrink.addEventListener("click", writeDrinkTitle);
 getFoodAndDrink.addEventListener("click", writeMealTitle);
